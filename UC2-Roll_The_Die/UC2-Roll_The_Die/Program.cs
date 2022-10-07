988a8d5c36d564c831a44fc40971dd4d3005621b
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-10-07 11:19:56 +0530

    Add files via upload

---
+++ UC2-Roll_The_Die/UC2-Roll_The_Die/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC2_Roll_The_Die
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Position = 0;
+            Console.WriteLine("WELCOME TO SNAKE AND LADDER PROGRAME");
+            Console.WriteLine($"You are at Starting Position:: {Position}");
+
+            //Roll_the_die_to_get_a_number
+            Random random = new Random();
+            int die = random.Next(1, 7);
+            Console.WriteLine($"You are at Starting Position:: {die}");
+            Console.ReadLine();
+        }
+    }
+}
