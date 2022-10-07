e5fec289f3feb4e30de7176e993457a55b1dc891
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-10-07 11:17:28 +0530

    Add files via upload

---
+++ Game_ Played_With_Single_Player/Game_ Played_With_Single_Player/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game__Played_With_Single_Player
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Position = 0;
+            Console.WriteLine("WELCOME TO SNAKE AND LADDER PROGRAME");
+            Console.WriteLine($"You are at Starting Position:: {Position}");
+            Console.ReadLine();
+        }
+    }
+}
